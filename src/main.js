ea0c8dc7f9f914b59925b02dcc7bb49cc840b478
lianhong.ou <lianhong.ou@thundersoft.com>
2023-02-05 20:16:29 +0800

    fix: xx

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ export default function (Vue, { router, head, isClient }) {
     Vue.mixin({
         data () {
             return {
-                // GRIDSOME_API_URL: process.env.GRIDSOME_API_URL
-                GRIDSOME_API_URL: 'http://localhost:1337'
+                GRIDSOME_API_URL: process.env.GRIDSOME_API_URL
+                // GRIDSOME_API_URL: 'http://localhost:1337'
             }
         }
     })
